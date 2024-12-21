26f25ed6f120eef48a696599cb533c3f7d31f591
Andrei Kvapil <kvapss@gmail.com>
2024-12-22 00:06:22 +0100

    tilde

--- cmd/kubeapps-apis/plugins/fluxv2/packages/v1alpha1/release.go
+++ cmd/kubeapps-apis/plugins/fluxv2/packages/v1alpha1/release.go
@@ -109,22 +109,23 @@ func (s *Server) paginatedInstalledPkgSummaries(ctx context.Context, headers htt
 func (s *Server) installedPkgSummaryFromRelease(ctx context.Context, headers http.Header, rel *unstructured.Unstructured) (*corev1.InstalledPackageSummary, error) {
 	name := rel.GetName()
 	namespace := rel.GetNamespace()
+	kind := rel.GetKind()
 	version, _, _ := unstructured.NestedString(rel.Object, "appVersion")
 
-	// Найти соответствующий helm chart через конфигурацию
+	// Find the resource config
 	var resourceConfig *common.ConfigResource
 	for _, res := range s.pluginConfig.Resources {
-		if res.Application.Kind == rel.GetKind() {
+		if res.Application.Kind == kind {
 			resourceConfig = &res
 			break
 		}
 	}
 
 	if resourceConfig == nil {
-		return nil, fmt.Errorf("Resource config not found for kind: %s", rel.GetKind())
+		return nil, fmt.Errorf("Resource config not found for kind: %s", kind)
 	}
 
-	// Создаем ссылку на доступный пакет (helm chart)
+	// Create reference with kind included in identifier
 	availablePkgRef := &corev1.AvailablePackageReference{
 		Context: &corev1.Context{
 			Namespace: resourceConfig.Release.Chart.SourceRef.Namespace,
@@ -136,11 +137,14 @@ func (s *Server) installedPkgSummaryFromRelease(ctx context.Context, headers htt
 		Plugin: GetPluginDetail(),
 	}
 
-	// Получаем детали helm chart'а
+	// Get package details
 	pkgDetail, err := s.getAvailablePackageDetail(ctx, headers, availablePkgRef)
 	if err != nil {
 		return nil, err
 	}
+
+	// Create identifier using ~ as separator
+	identifier := fmt.Sprintf("%s~%s", kind, name)
 
 	return &corev1.InstalledPackageSummary{
 		InstalledPackageRef: &corev1.InstalledPackageReference{
@@ -148,7 +152,7 @@ func (s *Server) installedPkgSummaryFromRelease(ctx context.Context, headers htt
 				Namespace: namespace,
 				Cluster:   s.kubeappsCluster,
 			},
-			Identifier: name,
+			Identifier: identifier,
 			Plugin:     GetPluginDetail(),
 		},
 		Name: name,
@@ -159,8 +163,7 @@ func (s *Server) installedPkgSummaryFromRelease(ctx context.Context, headers htt
 		IconUrl:          pkgDetail.IconUrl,
 		PkgDisplayName:   pkgDetail.DisplayName,
 		ShortDescription: pkgDetail.ShortDescription,
-		// Add reference to available package
-		Status: getResourceStatus(rel),
+		Status:           getResourceStatus(rel),
 	}, nil
 }
 
@@ -378,87 +381,122 @@ func (s *Server) updateRelease(ctx context.Context, headers http.Header, package
 
 // deleteRelease deletes a custom resource instance
 func (s *Server) deleteRelease(ctx context.Context, headers http.Header, packageRef *corev1.InstalledPackageReference) error {
-	// Get dynamic client with user auth
+	// Split identifier to get kind and name
+	parts := strings.Split(packageRef.Identifier, "~")
+	if len(parts) != 2 {
+		return fmt.Errorf("invalid identifier format: %s", packageRef.Identifier)
+	}
+	kind := parts[0]
+	name := parts[1]
+
+	// Find resource config
+	var resourceConfig *common.ConfigResource
+	for _, res := range s.pluginConfig.Resources {
+		if res.Application.Kind == kind {
+			resourceConfig = &res
+			break
+		}
+	}
+	if resourceConfig == nil {
+		return fmt.Errorf("resource config not found for kind: %s", kind)
+	}
+
+	// Get GVR
+	gvr := schema.GroupVersionResource{
+		Group:    "apps.cozystack.io",
+		Version:  "v1alpha1",
+		Resource: resourceConfig.Application.Plural,
+	}
+
+	// Get dynamic client
 	dynamicClient, err := s.clientGetter.Dynamic(headers, s.kubeappsCluster)
 	if err != nil {
 		return fmt.Errorf("failed to get dynamic client: %w", err)
 	}
 
-	gvr, err := s.getGVRFromPackageID(packageRef.Identifier)
+	// Delete the resource
+	err = dynamicClient.Resource(gvr).Namespace(packageRef.Context.Namespace).Delete(ctx, name, metav1.DeleteOptions{})
 	if err != nil {
-		return err
-	}
-
-	err = dynamicClient.Resource(gvr).Namespace(packageRef.Context.Namespace).Delete(ctx, packageRef.Identifier, metav1.DeleteOptions{})
-	if err != nil {
-		return connecterror.FromK8sError("delete", gvr.Resource, packageRef.Identifier, err)
+		return connecterror.FromK8sError("delete", gvr.Resource, name, err)
 	}
 
 	return nil
 }
 
 func (s *Server) installedPackageDetail(ctx context.Context, headers http.Header, key types.NamespacedName) (*corev1.InstalledPackageDetail, error) {
+	// Split the identifier to get kind and name
+	parts := strings.Split(key.Name, "~")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("invalid identifier format: %s", key.Name)
+	}
+	kind := parts[0]
+	name := parts[1]
+
 	// Get dynamic client with user auth
 	dynamicClient, err := s.clientGetter.Dynamic(headers, s.kubeappsCluster)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get dynamic client: %w", err)
 	}
 
-	gvrs := s.getAvailableResourceTypes()
-
-	for _, gvr := range gvrs {
-		obj, err := dynamicClient.Resource(gvr).Namespace(key.Namespace).Get(ctx, key.Name, metav1.GetOptions{})
-		if err == nil {
-			version, _, _ := unstructured.NestedString(obj.Object, "appVersion")
-			spec, _, _ := unstructured.NestedMap(obj.Object, "spec")
-			valuesJson, err := json.Marshal(spec)
-			if err != nil {
-				return nil, fmt.Errorf("failed to marshal spec: %w", err)
-			}
-
-			// Find the resource config
-			var resourceConfig *common.ConfigResource
-			for _, res := range s.pluginConfig.Resources {
-				if res.Application.Plural == gvr.Resource {
-					resourceConfig = &res
-					break
-				}
-			}
-			if resourceConfig == nil {
-				return nil, fmt.Errorf("resource config not found for %s", gvr.Resource)
-			}
-
-			availablePkgRef := &corev1.AvailablePackageReference{
-				Context: &corev1.Context{
-					Namespace: resourceConfig.Release.Chart.SourceRef.Namespace,
-					Cluster:   s.kubeappsCluster,
-				},
-				Identifier: fmt.Sprintf("%s/%s",
-					resourceConfig.Release.Chart.SourceRef.Name,
-					resourceConfig.Release.Chart.Name),
-				Plugin: GetPluginDetail(),
-			}
-
-			return &corev1.InstalledPackageDetail{
-				InstalledPackageRef: &corev1.InstalledPackageReference{
-					Context: &corev1.Context{
-						Namespace: key.Namespace,
-						Cluster:   s.kubeappsCluster,
-					},
-					Identifier: key.Name,
-					Plugin:     GetPluginDetail(),
-				},
-				Name: key.Name,
-				CurrentVersion: &corev1.PackageAppVersion{
-					PkgVersion: version,
-					AppVersion: version,
-				},
-				AvailablePackageRef: availablePkgRef,
-				ValuesApplied:       string(valuesJson),
-				Status:              getResourceStatus(obj),
-			}, nil
+	// Find the resource config for the kind
+	var resourceConfig *common.ConfigResource
+	for _, res := range s.pluginConfig.Resources {
+		if res.Application.Kind == kind {
+			resourceConfig = &res
+			break
 		}
 	}
+	if resourceConfig == nil {
+		return nil, fmt.Errorf("resource config not found for kind: %s", kind)
+	}
 
-	return nil, fmt.Errorf("package not found: %s", key.String())
+	// Get the GVR for the resource type
+	gvr := schema.GroupVersionResource{
+		Group:    "apps.cozystack.io",
+		Version:  "v1alpha1",
+		Resource: resourceConfig.Application.Plural,
+	}
+
+	// Get the resource
+	obj, err := dynamicClient.Resource(gvr).Namespace(key.Namespace).Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		return nil, connecterror.FromK8sError("get", gvr.Resource, name, err)
+	}
+
+	version, _, _ := unstructured.NestedString(obj.Object, "appVersion")
+	spec, _, _ := unstructured.NestedMap(obj.Object, "spec")
+	valuesJson, err := json.Marshal(spec)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal spec: %w", err)
+	}
+
+	availablePkgRef := &corev1.AvailablePackageReference{
+		Context: &corev1.Context{
+			Namespace: resourceConfig.Release.Chart.SourceRef.Namespace,
+			Cluster:   s.kubeappsCluster,
+		},
+		Identifier: fmt.Sprintf("%s/%s",
+			resourceConfig.Release.Chart.SourceRef.Name,
+			resourceConfig.Release.Chart.Name),
+		Plugin: GetPluginDetail(),
+	}
+
+	return &corev1.InstalledPackageDetail{
+		InstalledPackageRef: &corev1.InstalledPackageReference{
+			Context: &corev1.Context{
+				Namespace: key.Namespace,
+				Cluster:   s.kubeappsCluster,
+			},
+			Identifier: fmt.Sprintf("%s~%s", kind, name),
+			Plugin:     GetPluginDetail(),
+		},
+		Name: name,
+		CurrentVersion: &corev1.PackageAppVersion{
+			PkgVersion: version,
+			AppVersion: version,
+		},
+		AvailablePackageRef: availablePkgRef,
+		ValuesApplied:       string(valuesJson),
+		Status:              getResourceStatus(obj),
+	}, nil
 }
